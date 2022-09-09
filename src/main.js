2d6262be6303fe9b81e59c17595a91f49f91725b
Jwade1781 <jwade5219@gmail.com>
2022-09-09 16:32:38 -0400

    Basic JavaScript implemenetation for adding books

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 function book() { }
-book.prototype.consoleTables = function () { console.table(this) }
+book.prototype.printBook = function () { console.table(this) }
+
+function library() { }
+library.prototype.books = [];
+library.prototype.printBooks = function () { console.table(this.books) }
+
+const readLibrary = new library();
+const readingLibrary = new library();
+const backlogLibrary = new library();
+
 
 function addBook() {
     const newBook = new book();
@@ -7,47 +16,70 @@ function addBook() {
     newBook.bookName = document.querySelector("#bookName").value;
     newBook.pagesRead = document.querySelector("#pagesRead").value;
     newBook.readStatus = document.querySelector("#readStatus").value;
-    //newBook.consoleTables();
-
-    appendLibrary(newBook);
-}
-
-function appendLibrary(newBook) {
-    const divBookStruct = {
+    newBook.bookStructure = {
         "bookName": {
             "tag": "h2",
             "class": "bookName",
-            "value": newBook.bookName,
+            "value": this.bookName.value,
         },
 
         "authorName": {
             "tag": "h4",
             "class": "authorName",
-            "value": newBook.authorName,
+            "value": this.authorName.value,
         },
 
         "pagesRead": {
             "tag": "h5",
             "class": "pagesRead",
-            "value": newBook.pagesRead,
+            "value": this.pagesRead.value,
         },
 
         "readStatus": {
             "tag": "h6",
             "class": "readStatus",
-            "value": newBook.readStatus,
+            "value": this.readStatus.value,
         }
-    };
+    }
 
-    const booksDiv = document.querySelector("#books");
+    let appendedLibrary = appendLibrary(newBook);
+    createElements(newBook);
+    const booksDivId = "#" + newBook.bookStructure["readStatus"]["value"].replace(/ /g, '').toLowerCase();
+    editPage(booksDivId + "BooksMainHeader", booksDivId + "BooksSubHeader", appendedLibrary);
+}
+
+function appendLibrary(newBook) {
+    switch (newBook.readStatus) {
+        case "Finished":
+            console.log("Finished")
+            readLibrary.books.push(newBook);
+            readLibrary.printBooks();
+            return readLibrary;
+        case "Reading":
+            console.log("Reading")
+            readingLibrary.books.push(newBook);
+            readingLibrary.printBooks();
+            return readingLibrary;
+        case "Not Started":
+            console.log("Backlog")
+            backlogLibrary.books.push(newBook);
+            backlogLibrary.printBooks();
+            return backlogLibrary;
+        default:
+            console.log("Something broke")
+            return null;
+    }
+}
+
+function createElements(newBook) {
     let parentDiv = document.createElement("div");
     parentDiv.classList.add("book");
-    for (let [outterKey, outterValue] of Object.entries(divBookStruct)) {
+    for (let [outterKey, outterValue] of Object.entries(newBook.bookStructure)) {
         let element;
         let readStatusFound = false;
 
         for (let [innerKey, innerValue] of Object.entries(outterValue)) {
-            switch(innerKey){
+            switch (innerKey) {
                 case "tag":
                     element = document.createElement(innerValue);
                     break;
@@ -59,17 +91,17 @@ function appendLibrary(newBook) {
                     element.value = innerValue;
                     element.textContent = element.value;
                     if (readStatusFound) {
-                        innerValue = 
-                        innerValue = innerValue.replace(/ /g,'').toLowerCase();
+                        innerValue = innerValue.replace(/ /g, '').toLowerCase();
                         parentDiv.classList.add(innerValue);
                     }
                     break;
             }
             parentDiv.appendChild(element);
         }
-
-
     }
+    const booksDivId = "#" + newBook.bookStructure["readStatus"]["value"].replace(/ /g, '').toLowerCase() + "Books";
+    const booksDiv = document.querySelector(booksDivId);
+
     let editButtonsDiv = document.createElement("div");
     editButtonsDiv.classList.add("editButtons");
 
@@ -78,13 +110,24 @@ function appendLibrary(newBook) {
     deleteButton.textContent = "Delete";
     editButton.textContent = "Edit";
     deleteButton.classList.add("darkHover");
+    deleteButton.addEventListener("click", () => {
+        booksDiv.removeChild(parentDiv);
+    })
     editButton.classList.add("darkHover");
-
     editButtonsDiv.appendChild(editButton);
     editButtonsDiv.appendChild(deleteButton);
     parentDiv.appendChild(editButtonsDiv);
-
     booksDiv.appendChild(parentDiv);
+}
+
+function editPage(mainHeaderId, subHeaderId, Library) {
+    const mainHeaderElement = document.querySelector(mainHeaderId);
+    const subHeaderElement = document.querySelector(subHeaderId);
+    if (subHeaderElement.textContent) subHeaderElement.textContent = "";
+
+    let bookStatus = mainHeaderElement.textContent;
+    bookStatus = bookStatus.substring(0, bookStatus.indexOf("(")) + "(" + Library.books.length + ")";
+    mainHeaderElement.textContent = bookStatus;
 }
 
 
